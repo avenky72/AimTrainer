18a0825bf13a8a23eae118139f2e6432661aefee
Atul Venkatesan <atulvenky@gmail.com>
2024-07-18 16:34:32 -0700

    outline of functions

--- AimTrainer.py
+++ AimTrainer.py
@@ -9,6 +9,17 @@ class AimTrainer:
         self.canvas = tk.Canvas(root, width=800, height=800, bg="beige")
         self.canvas.pack()
 
+    # Periodically add target
+    def create_target():
+        return None
+
+    # Remove target after 3 seconds
+    def remove_target():
+        return None
+    
+    # If the target is clicked via MouseClick, increase the point total and remove target
+    def hit():
+        return None
 
 
 if __name__ == "__main__":
