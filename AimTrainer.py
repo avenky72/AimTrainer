de08b27667a28a8b382e06ebd8e46084fdd42061
Atul Venkatesan <atulvenky@gmail.com>
2024-07-24 16:54:49 -0700

    creation, display, remove, and hit targets work next need to add timing of when targets appear and when they disappear also speed up as points increase

--- AimTrainer.py
+++ AimTrainer.py
@@ -1,33 +1,63 @@
 import tkinter as tk
 import random
+import math
 
 
-class AimTrainer:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("PC Aim Trainer by Atul Venky")
-        self.canvas = tk.Canvas(root, width=800, height=800, bg="beige")
-        self.canvas.pack()
+root = tk.Tk()
+root.title("Aim Trainer")
+canvas = tk.Canvas(root, width=800, height=800, bg="beige")
+canvas.pack()
+#global points
+points = 0
+targets = {}
+r = 50
 
-    # Periodically add target
-    def create_target():
-        return None
+# Periodically add target
+def create_target():
+    # Random color 
+    def randColor():
+        return f'#{random.randint(0, 0xFFFFFF):06x}'
 
-    # Remove target after 3 seconds
-    def remove_target():
-        return None
+    x = random.randint(100, 700)
+    y = random.randint(100, 700)
+    color = randColor()
+    target = canvas.create_oval(x - r, y - r, x + r, y + r, fill=color, outline=color)
+    targets[target] = {'x': x, 'y': y, 'r': r}
+    print(targets)
+
+
+# Remove target after 3 seconds
+def remove_target(target):
+    if target in targets:
+        canvas.delete(target)
+        del targets[target]
+
+def get_click_coordinates(event):
+    a, b = event.x, event.y
     
     # If the target is clicked via MouseClick, increase the point total and remove target
-    def hit():
-        return None
+    def hit(target):
+        hit_target = targets[target]
+        target_x, target_y, radius = hit_target['x'], hit_target['y'], hit_target['r']
+        in_target = math.sqrt((a - target_x) ** 2 + (b - target_y) ** 2)
+        return in_target <= radius
+
+    for t in targets:
+        if hit(t):
+            global points
+            points += 1
+            print(points)
+            remove_target(t)
+            if not targets:
+                break
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
 
-    game = AimTrainer(root)
-    
-    
-    
-    
-    root.mainloop()
+create_target()
+create_target()
+create_target()
+create_target()
+
+canvas.bind("<Button-1>", get_click_coordinates)
+
+root.mainloop()
